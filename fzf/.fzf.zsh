3014620082fa7a817d7bf17d1d38af8eb9c6749c
welly <im43089@gmail.com>
2024-06-16 04:54:07 +0800

    fzf setup

---
+++ fzf/.fzf.zsh
@@ -0,0 +1,15 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/welly/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/home/welly/.fzf/bin"
+fi
+
+export FZF_DEFAULT_COMMAND="rg --line-number --with-filename . --color=always --hidden --files | fzf --delimiter : --preview \"bat --color=always {1} --highlight-line {2}\" --preview-window ~8,+{2}-5"
+
+export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS' 
+  --color=fg:#c0caf5,bg:#1a1b26,hl:#bb9af7
+  --color=fg+:#c0caf5,bg+:#1a1b26,hl+:#7dcfff
+  --color=info:#7aa2f7,prompt:#7dcfff,pointer:#7dcfff 
+  --color=marker:#9ece6a,spinner:#9ece6a,header:#9ece6a'
+
+source <(fzf --zsh)
