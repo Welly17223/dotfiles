54b65b72215a3a962f062af9ae48a1443db3e7d6
welly <im43089@gmail.com>
2024-06-22 18:45:06 +0800

    auto install stow script

--- down_stow.sh
+++ down_stow.sh
@@ -1,8 +1,13 @@
 # !/bin/bash
 
+CURDIR=$(pwd)
+mkdir -p ./_temp
+cd ./_temp
 curl -OL https://ftp.gnu.org/gnu/stow/stow-latest.tar.gz
 tar -xzf stow-latest.tar.gz
+rm stow-latest.tar.gz
 cd stow-*
 ./configure && sudo make install
 cd ..
-rm -rf stow-latest stow-latest.tar.gz
+cd $CURDIR
+rm -rf ./_temp
