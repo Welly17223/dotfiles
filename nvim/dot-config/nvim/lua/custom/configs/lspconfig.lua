cd50dea63da813e545d345f07cc1aa3d0baa051c
welly <im43089@gmail.com>
2024-08-15 17:06:56 +0800

    somechange

--- nvim/dot-config/nvim/lua/custom/configs/lspconfig.lua
+++ nvim/dot-config/nvim/lua/custom/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "cssls", "tsserver", "pyright", "pylsp", "vimls", "texlab" }
+local servers = { "cssls", "tsserver", "pyright", "pylsp", "vimls", "texlab", "cmake" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
