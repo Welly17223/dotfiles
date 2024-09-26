04b758fcc83d323b09c5ea2ab46780068cedeaf4
welly <im43089@gmail.com>
2024-09-26 12:03:29 +0800

    add verilog lsp to neovim config

--- nvim/dot-config/nvim/lua/custom/configs/lspconfig.lua
+++ nvim/dot-config/nvim/lua/custom/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require "lspconfig"
 
 -- if you just want default config for the servers then put them in a table
-local servers = { "cssls", "tsserver", "html", "clangd", "pyright", "arduino_language_server" }
+local servers = { "cssls", "tsserver", "html", "clangd", "pyright", "arduino_language_server", "lemminx", "verible" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
@@ -29,3 +29,30 @@ lspconfig.grammarly.setup {
 lspconfig.ltex.setup {}
 lspconfig.pylsp.setup {}
 lspconfig.vimls.setup {}
+
+lspconfig.svlangserver.setup {
+  on_init = function(client)
+    local path = client.workspace_folders[1].name
+
+    if path == '/path/to/project1' then
+      client.config.settings.systemverilog = {
+        includeIndexing     = {"**/*.{sv,svh}"},
+        excludeIndexing     = {"test/**/*.sv*"},
+        defines             = {},
+        launchConfiguration = "/tools/verilator -sv -Wall --lint-only",
+        formatCommand       = "/tools/verible-verilog-format"
+      }
+    elseif path == '/path/to/project2' then
+      client.config.settings.systemverilog = {
+        includeIndexing     = {"**/*.{sv,svh}"},
+        excludeIndexing     = {"sim/**/*.sv*"},
+        defines             = {},
+        launchConfiguration = "/tools/verilator -sv -Wall --lint-only",
+        formatCommand       = "/tools/verible-verilog-format"
+      }
+    end
+
+    client.notify("workspace/didChangeConfiguration")
+    return true
+  end
+}
