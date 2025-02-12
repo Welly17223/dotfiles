5745e812fd28bcc8502875d55c2553a3c0dae691
welly <im43089@gmail.com>
2025-02-12 17:03:02 +0800

    remove powershell clipboard, and switch default python lsp to pylsp

--- nvim/dot-config/nvim/lua/configs/lspconfig.lua
+++ nvim/dot-config/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspcg = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "ltex", "texlab", "pyright", "cmake", "lwc-language-server", "lemminx","arduino_language_server"  }
+local servers = { "html", "cssls", "ltex", "texlab", "cmake", "lemminx","arduino_language_server", "pylsp"  }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
