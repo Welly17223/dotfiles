f5c732c51afe2b9db0c39bdf306069d37ae9f4eb
welly <im43089@gmail.com>
2025-03-28 14:28:06 +0800

    typescript lsp

--- nvim/dot-config/nvim/lua/configs/lspconfig.lua
+++ nvim/dot-config/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspcg = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "ltex", "texlab", "cmake", "lemminx","arduino_language_server", "pylsp", "asm_lsp"  }
+local servers = { "html", "cssls", "ltex", "texlab", "cmake", "lemminx","arduino_language_server", "pylsp", "asm_lsp", "ts_ls"  }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
