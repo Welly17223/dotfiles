248a8af3c9570249587d51167261d1ddc4626230
Welly_Hsu <im43089@gmail.com>
2025-04-02 02:07:50 +0800

    zinit replace zplug

--- zsh/dot-zsh/plugin.zsh
+++ zsh/dot-zsh/plugin.zsh
@@ -1,16 +1,17 @@
-# Check if zplug is installed
-if [[ ! -d ~/.zplug ]]; then
-  git clone https://github.com/zplug/zplug ~/.zplug
-  source ~/.zplug/init.zsh && zplug update --self
-fi
+ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
+[ ! -d $ZINIT_HOME ] && mkdir -p "$(dirname $ZINIT_HOME)"
+[ ! -d $ZINIT_HOME/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
+source "${ZINIT_HOME}/zinit.zsh"
 
-source ~/.zplug/init.zsh
+zinit ice depth"1" as"theme"; zinit light romkatv/powerlevel10k
+zinit ice wait depth"1" lucid; zinit light zsh-users/zsh-completions
 
-zplug "romkatv/powerlevel10k", as:theme, depth:1
-zplug 'zplug/zplug', hook-build:'zplug --self-manage'
-zplug "zsh-users/zsh-completions"
+# zplug "romkatv/powerlevel10k", as:theme, depth:1
+# zplug 'zplug/zplug', hook-build:'zplug --self-manage'
+# zplug "zsh-users/zsh-completions"
 
-zplug "zsh-users/zsh-history-substring-search", as:plugin
+# zplug "zsh-users/zsh-history-substring-search", as:plugin
+zinit ice wait depth"1" lucid; zinit light zsh-users/zsh-history-substring-search
 bindkey '^[OA' history-substring-search-up
 bindkey '^[[A' history-substring-search-up
 bindkey '^[OB' history-substring-search-down
@@ -20,20 +21,18 @@ bindkey -M emacs '^N' history-substring-search-down
 HISTORY_SUBSTRING_SEARCH_PREFIXED=1
 HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE=1
 
-zplug "zsh-users/zsh-autosuggestions"
-zplug "zdharma/fast-syntax-highlighting"
-zplug "zpm-zsh/ls"
-zplug "agkozak/zsh-z"
-zplug "MichaelAquilina/zsh-you-should-use"
-zplug "Aloxaf/fzf-tab"
+zinit ice wait"1" depth"1" lucid; zinit load Aloxaf/fzf-tab
+zinit ice wait depth"1" lucid; zinit load zsh-users/zsh-autosuggestions
+zinit ice wait depth"1" lucid; zinit load zdharma/fast-syntax-highlighting
+zinit ice wait depth"1" lucid; zinit load zpm-zsh/ls
+zinit ice wait depth"1" lucid; zinit load agkozak/zsh-z
+zinit ice wait depth"1" lucid; zinit load MichaelAquilina/zsh-you-should-use
 
+# zplug "Aloxaf/fzf-tab"
+# zplug "zsh-users/zsh-autosuggestions"
+# zplug "zdharma/fast-syntax-highlighting"
+# zplug "zpm-zsh/ls"
+# zplug "agkozak/zsh-z"
+# zplug "MichaelAquilina/zsh-you-should-use"
 # zplug "marlonrichert/zsh-autocomplete"
-
-if ! zplug check --verbose; then
-    printf "Install? [y/N]: "
-    if read -q; then
-        echo; zplug install
-    fi
-fi
-zplug load
 
