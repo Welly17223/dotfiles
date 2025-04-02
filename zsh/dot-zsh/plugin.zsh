71fe1a4fdb964facc53381c3f44bacbbd9010b9f
welly <im43089@gmail.com>
2025-04-02 15:26:05 +0800

    improve zsh speed

--- zsh/dot-zsh/plugin.zsh
+++ zsh/dot-zsh/plugin.zsh
@@ -3,7 +3,7 @@ ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
 [ ! -d $ZINIT_HOME/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
 source "${ZINIT_HOME}/zinit.zsh"
 
-zinit ice depth"1" as"theme"; zinit light romkatv/powerlevel10k
+zinit ice depth"1" as"theme" lucid; zinit light romkatv/powerlevel10k
 # zinit ice wait depth"1" lucid; zinit light zsh-users/zsh-completions
 
 zinit wait lucid for \
