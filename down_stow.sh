dd7136302a8a32d89e1aa470b0df654500c80044
welly <im43089@gmail.com>
2025-04-13 23:40:57 +0800

    let finish shell scipt pass shellcheck

--- down_stow.sh
+++ down_stow.sh
@@ -1,13 +1,13 @@
-# !/bin/bash
+#!/bin/bash
 
 CURDIR=$(pwd)
 mkdir -p ./_temp
-cd ./_temp
+cd ./_temp || (echo "Failed to change directory to _temp" && exit 1)
 curl -OL https://ftp.gnu.org/gnu/stow/stow-latest.tar.gz
 tar -xzf stow-latest.tar.gz
 rm stow-latest.tar.gz
-cd stow-*
+cd stow-* || (echo "Failed to change directory to _temp" && exit 1)
 ./configure && sudo make install
-cd ..
-cd $CURDIR
+cd .. || (echo "Failed to change directory to _temp" && exit 1)
+cd "$CURDIR" || (echo "Failed to change directory to _temp" && exit 1)
 rm -rf ./_temp
--- impl_all.sh
+++ impl_all.sh
@@ -6,6 +6,6 @@ for i in ./*; do
   if [[ -d "$i" ]]; then
     # stow ${i}
     echo "stow ${i##*/}";
-    stow ${i##*/} --dotfiles;
+    stow "${i##*/}" --dotfiles;
   fi
 done
