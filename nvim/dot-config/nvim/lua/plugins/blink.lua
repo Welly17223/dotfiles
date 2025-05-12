47494f4f21407f76e5f370545b956d891dfb8836
welly <im43089@gmail.com>
2025-05-13 04:46:15 +0800

    blink cmp

---
+++ nvim/dot-config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,20 @@
+return {
+  { import = "nvchad.blink.lazyspec" },
+  {
+    "saghen/blink.cmp",
+    dependencies = { "fang2hou/blink-copilot" },
+    opts = {
+      sources = {
+        default = { "copilot" },
+        providers = {
+          copilot = {
+            name = "copilot",
+            module = "blink-copilot",
+            score_offset = 100,
+            async = true,
+          },
+        },
+      },
+    },
+  },
+}
--- nvim/dot-config/nvim/lua/plugins/copilot.lua
+++ nvim/dot-config/nvim/lua/plugins/copilot.lua
@@ -1,7 +1,25 @@
 return {
   "CopilotC-Nvim/CopilotChat.nvim",
   dependencies = {
-    { "github/copilot.vim", lazy = false }, -- or zbirenbaum/copilot.lua
+    {
+      "github/copilot.vim",
+      cmd = "Copilot",
+      event = "BufWinEnter",
+      init = function()
+        vim.g.copilot_no_maps = true
+      end,
+      config = function()
+        -- Block the normal Copilot suggestions
+        vim.api.nvim_create_augroup("github_copilot", { clear = true })
+        vim.api.nvim_create_autocmd({ "FileType", "BufUnload" }, {
+          group = "github_copilot",
+          callback = function(args)
+            vim.fn["copilot#On" .. args.event]()
+          end,
+        })
+        vim.fn["copilot#OnFileType"]()
+      end,
+    }, -- or zbirenbaum/copilot.lua
     { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
   },
   build = "make tiktoken", -- Only on MacOS or Linux
