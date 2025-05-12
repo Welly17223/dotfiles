35bfc52241249ac67714aa9d7405f18654b33108
welly <im43089@gmail.com>
2025-05-13 04:46:25 +0800

    todo comment plugins

---
+++ nvim/dot-config/nvim/lua/plugins/todo-comments.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+    keywords = {
+      NOTE = { alt = { "NOTE", "HINt", "Hint" } },
+    },
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
+  lazy = false,
+}
