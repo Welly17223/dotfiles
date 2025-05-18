07c0159a60eaa97cb01c711131b9386e6f7c14d8
Welly_Hsu <im43089@gmail.com>
2025-05-18 21:40:57 +0800

    new right click

--- nvim/dot-config/nvim/lua/mappings.lua
+++ nvim/dot-config/nvim/lua/mappings.lua
@@ -16,4 +16,22 @@ map({ "n", "t" }, "<A-f>", function()
   require("nvchad.term").toggle { pos = "sp", id = "htoggleTerm" }
 end, { desc = "terminal toggleable horizontal term" })
 
+-- Keyboard users
+vim.keymap.set("n", "<C-t>", function()
+  require("menu").open("default")
+end, {})
+
+-- mouse users + nvimtree users!
+vim.keymap.set({ "n", "v" }, "<RightMouse>", function()
+  require('menu.utils').delete_old_menus()
+
+  vim.cmd.exec '"normal! \\<RightMouse>"'
+
+  -- clicked buf
+  local buf = vim.api.nvim_win_get_buf(vim.fn.getmousepos().winid)
+  local options = vim.bo[buf].ft == "NvimTree" and "nvimtree" or "default"
+
+  require("menu").open(options, { mouse = true })
+end, {})
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
